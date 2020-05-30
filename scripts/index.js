8b79c77685cd77a8e0507550f23a780a8618a6b2
Rupa <rupa211984@gmail.com>
2020-05-29 18:23:23 -0700

    Make cat sounds

--- scripts/index.js
+++ scripts/index.js
@@ -3,11 +3,15 @@
 //   console.log(document.querySelector('form'));
 // });
 
+// Make the form add cat gifs and comments
 const form = document.querySelector('form');
 
+// How can we make this code friendlier to read? 
+// What if we separated it out into functions?
 form.addEventListener('submit', function () {
   event.preventDefault();
 
+  // Why am I using so many consts? Why not let?
   const gifURL = form.querySelector('#gif-url').value;
   const comment = form.querySelector('#comment').value;
 
@@ -24,4 +28,61 @@ form.addEventListener('submit', function () {
   posts.append(div);
 
   event.currentTarget.reset();
+});
+
+// Let's see how events bubble and capture (trickle)
+// Grab all cat actions elements (section, h2, div, buttons)
+const catActions = document.querySelectorAll('.actions, .actions *');
+
+// UNCOMMENT TO SEE CAPTURING IN ACTION
+// for (const element of catActions) {
+//   element.addEventListener('click', function () {
+//     console.log(`Capture says you clicked ${event.currentTarget.tagName }`);
+//   }, true);
+// }
+
+// UNCOMMENT TO SEE BUBBLING IN ACTION
+// for (const element of catActions) {
+//   element.addEventListener('click', function () {
+//     console.log(`Bubble says you clicked ${event.currentTarget.tagName }`);
+//   });
+// }
+
+// WHAT IF YOU UNCOMMENT BOTH OF THE FOR LOOPS ABOVE??
+
+// LET'S CHECK OUT event.stopPropagation()
+const meow = document.querySelector('#meow');
+
+// UNCOMMENT ALL OF THE CODE BELOW TO SEE WHAT HAPPENS
+// meow.addEventListener('click', function () {
+//   event.stopPropagation();
+//   console.log('meow');
+// });
+
+// const actionsSection = document.querySelector('.actions');
+
+// actionsSection.addEventListener('click', function () {
+//   console.log('This is the actions section element speaking');
+// });
+
+// WHAT HAPPENS IF YOU COMMENT OUT event.stopPropagation() IN THE ABOVE CODE?
+// WHAT HAPPENS IF YOU UNCOMMENT event.stopPropagation() BUT SET THE actionsSection LISTENER TO CAPTURE?
+// WHY DO YOU THINK THAT HAPPENED?
+
+// Event listeners and performance, let's make our cat actions buttons work
+// with only one listener, oooohhh, aaaaaahhhh, such wow
+const catActionsSection = document.querySelector('.actions');
+
+catActionsSection.addEventListener('click', function () {
+  switch(event.target.id) {
+    case 'meow':
+      console.log('meow');
+      break;
+    case 'purr':
+      console.error('purr');
+      break;
+    case 'howl':
+      alert('awooooo');
+      break;
+  }
 });
